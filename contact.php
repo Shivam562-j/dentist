f6b7509ad78e5809e4ded93f40d9cef3b9e5feb6
Shivam562-j <76731566+Shivam562-j@users.noreply.github.com>
2023-02-17 15:25:47 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,17 +1,17 @@
 <?php
-    $name = $_REQUEST['name'];
-    $mobileno = $_REQUEST['mobileno'];
-    $email = $_REQUEST['email'];
-    $message = $_REQUEST['message'];
+    $name = $_REQUEST['Name'];
+    $mobileno = $_REQUEST['Mobileno'];
+    $email = $_REQUEST['Email'];
+    $message = $_REQUEST['Message'];
 
     // check the field
-    if(empty($name) || empty($mobileno) || empty($message)){
+    if(empty($name) || empty($email) || empty($mobileno) || empty($message)){
         echo "Please Fill all the field";
     }
 
     else{
-        mail("krshivam998@gmail.com" , "Dentist Website Message", $message , "From: $name <$email>");
-        echo "<script type="text/javadcript">alert("Your Message sent Sucessfully");
+        mail("krshivam998@gmail.com" , Dentist Website Message, $message , "From: $name <$mobileno> <$email>");
+        echo "<script type="text/javascript">alert("Your Message sent Sucessfully");
         window.history.log(-1)
 ;        </script>
     }
